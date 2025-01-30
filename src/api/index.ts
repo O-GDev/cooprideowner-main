df9a5f584fea4317541f94c707cff031b9f950a7
O-GDev <105860521+O-GDev@users.noreply.github.com>
2025-01-31 00:22:08 +0100

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,10 +6,10 @@ const createAxiosInstance = (axios: any, config: any) => {
 
   instance.interceptors.request.use(
     async (config: any) => {
-      // const token = await getCachedData("token");
-      // if (token) {
-      //   config.headers["Authorization"] = "Bearer " + token;
-      // }
+      const token = await getCachedData("token");
+      if (token) {
+        config.headers["Authorization"] = "Bearer " + token;
+      }
       return config;
     },
     (error: any) => {
